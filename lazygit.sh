9f5f9aacc6a825fc49eee18bd5f5a2e6903ec821
Spacemoose <daniel.glorioso@gmail.com>
2011-06-01 03:01:48 -0500

    lazygit.sh: zzzzzz

--- lazygit.sh
+++ lazygit.sh
@@ -2,6 +2,7 @@
 
 DATE=$(date +%m%d)
 TIME=$(date +%I%M)
+
 cd ~/github/android-hc-system-cdma
 zip -r hc_system_backup_old.zip . 
 rm ../hc_system_backup_old.zip
@@ -12,6 +13,7 @@ rm ~/github/android-hc-system-cdma/lazygit.sh
 mv hc_system_backup_old.zip ../hc_system_backup_old.zip
 cp -r ~/galaxytab/cdmabuild/android-hc-system-cdma/ ~/github/
 zip -r zip.zip .
-mv zip.zip ~/galaxytab/cdmabuild/android-hc-system-cdma/hc-test-"$DATE""$TIME".zip
+mv zip.zip ~/galaxytab/cdmabuild/hc-cdma-test-"$DATE""$TIME".zip
 git add -A
 git commit
+git push origin
